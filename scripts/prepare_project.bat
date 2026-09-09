eb5440df048f02b23bbda82c376cb6d80cdd3400
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-09 12:26:58 +0200

    CIDEX Mobile etap 4: przygotuj uprawnienia Android

--- scripts/prepare_project.bat
+++ scripts/prepare_project.bat
@@ -18,30 +18,36 @@ if errorlevel 1 (
 )
 
 echo ================================================
-echo   CIDEX Mobile - generowanie projektu Android
+echo   CIDEX Mobile - przygotowanie Android
 echo ================================================
 echo.
 
 if not exist "android\" (
-  echo [1/3] Generowanie plikow platformy Android...
+  echo [1/4] Generowanie plikow platformy Android...
   call "%FLUTTER%" create --platforms=android --org pl.cidex --project-name cidex_mobile .
   if errorlevel 1 goto :fail
 ) else (
-  echo [1/3] Folder android juz istnieje - pomijam generowanie.
+  echo [1/4] Folder android juz istnieje - pomijam generowanie.
 )
 
-echo [2/3] Wgrywanie interfejsu CIDEX Mobile...
+echo [2/4] Wgrywanie CIDEX Mobile...
 if not exist "lib" mkdir "lib"
 copy /Y "template\main.dart" "lib\main.dart" >nul
 copy /Y "template\pubspec.yaml" "pubspec.yaml" >nul
 
-echo [3/3] Pobieranie zaleznosci...
+echo [3/4] Ustawianie INTERNET, CAMERA i lokalnego HTTP...
+python "scripts\patch_android.py"
+if errorlevel 1 goto :fail
+
+echo [4/4] Pobieranie zaleznosci...
 call "%FLUTTER%" pub get
 if errorlevel 1 goto :fail
 
 echo.
 echo ================================================
 echo GOTOWE.
+echo Emulator: CIDEX API = http://10.0.2.2:8765
+echo Token wpisz w ustawieniach aplikacji.
 echo Teraz uruchom: scripts\run_emulator.bat
 echo ================================================
 echo.
