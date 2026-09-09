f56270293ccf4ac9403b08df762e21f309be0b9f
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-09 12:52:12 +0200

    CIDEX Mobile etap 6: test formularza Planisty

--- template/widget_test.dart
+++ template/widget_test.dart
@@ -1,7 +1,14 @@
+import 'package:cidex_mobile/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('CIDEX Mobile test bazowy', () {
-    expect('CIDEX Mobile'.contains('CIDEX'), isTrue);
+  test('CIDEX Mobile ma klienta API i formularz dodawania zlecenia', () {
+    const config = ApiConfig(baseUrl: 'http://10.0.2.2:8765', token: 'test');
+    final api = CidexApi(config);
+    final screen = AddOrderScreen(api: api);
+
+    expect(api.baseUrl, 'http://10.0.2.2:8765');
+    expect(screen.api, same(api));
+    expect(fmtNumber(5.0), '5');
   });
 }
