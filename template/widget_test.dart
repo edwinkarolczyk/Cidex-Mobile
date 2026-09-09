2c3ea73e74aec146bf47ba5e862be8711ae531c3
edwinkarolczyk <edwinkarolczyk@gmail.com>
2026-09-09 13:46:29 +0200

    WMM: aktualizuj nazwę testu aplikacji

--- template/widget_test.dart
+++ template/widget_test.dart
@@ -2,8 +2,8 @@ import 'package:cidex_mobile/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('CIDEX Mobile ma klienta API i formularz dodawania zlecenia', () {
-    const config = ApiConfig(baseUrl: 'http://10.0.2.2:8765', token: 'test');
+  test('Warsztat Menager Mobile ma klienta API i formularz dodawania zlecenia', () {
+    const config = ApiConfig(baseUrl: 'http://10.0.2.2:8765', token: 'ABC123');
     final api = CidexApi(config);
     final screen = AddOrderScreen(api: api);
 
